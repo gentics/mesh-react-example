428f747d56e21fd5d79e21241fd9a807d07291ab
Philipp Gürtler <p.guertler@gentics.com>
2018-06-15 16:44:55 +0200

    Show simple image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MeshReact from 'react-mesh';
+// import Folder from './schemas/Folder';
 
 const Mesh = MeshReact({
   baseUrl: "/api/v1",
-  projectName: "demo"
+  projectName: "demo",
+  imageBreakpoints: [768, 1024, 1920]
 })
 
 export default function App() {
   return (
-    <Router>
-      <Route path="/" render={route => (
-        <Mesh.Webroot path={route.location.pathname} />
-      )} />
-    </Router>
+    <Mesh.Image path="/images/yacht-pelorus.jpg" />
   )
 }
---
+++ src/schemas/Folder.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Folder() {
+  return (
+    <div>Hello World!</div>
+  )
+}
